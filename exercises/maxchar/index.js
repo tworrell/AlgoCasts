dd5cf74d0dae357b991d7a059ba1d59e98ecb71b
tworrell <tjworrell1@gmail.com>
2021-08-26 20:05:28 -0400

    adding my max characters solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let newStr = str.toLowerCase()
+    let charObj = {}
+    let max = 0
+    let maxChar = ''
+
+    for(let char of newStr) {
+        if(!charObj[char]) {
+            charObj[char] = 1
+        } else {
+            charObj[char]++
+        }
+    }
+
+    for(let char in charObj) {
+        if(charObj[char] > max) {
+            max = charObj[char]
+            maxChar = char
+        }
+    }
+    return maxChar
+}
 
 module.exports = maxChar;
